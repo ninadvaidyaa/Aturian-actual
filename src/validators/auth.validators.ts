72a3ff3fae4e43305fbdc138be88bec0927943e4
Shivendra Bind <128048250+ShBind@users.noreply.github.com>
2023-05-15 17:05:49 +0530

    fix: allow username and email in login field. (#25)
    fix: allow username and email in login field

--- src/validators/auth.validators.ts
+++ src/validators/auth.validators.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const loginFormSchema = z.object({
-  username: z.string().email(),
+  username: z.string(),
   password: z.string().min(3, "Password is required"),
 });
 export type LoginFormData = z.infer<typeof loginFormSchema>;
