e6ea3e81237df47227c805de9940b3759340de48
ShBind <shivendra.bind@newscapeconsulting.com>
2023-05-24 15:25:44 +0530

    chore: cleanup

--- src/pages/orders/index.tsx
+++
@@ -1,6 +0,0 @@
-// import { defaultColumns } from "./columnDefinition";
-// import { TableProvider } from "contexts/TableContext";
-import OrderPageComponent from "./OrderPage";
-
-const OrderPage = () => <OrderPageComponent />;
-export default OrderPage;
